798616e23e9c1e273a2fb3e91ac41614f850f4e6
Hritik Tailor <hritiktailor@gmail.com>
2023-10-30 11:51:18 +0000

    added bubble sort method

--- ssl_1.py
+++ ssl_1.py
@@ -20,13 +20,27 @@ class ssl():
         for i in self.singleList:
             print(i)
         
+    
+    def sort(self):
+        size = len(self.singleList)
+        swapped = False
+
+        for index in range(size - 1, 0, -1):
+            for i in range(index):
+                if self.singleList[i] > self.singleList[i + 1]:
+                    swapped = True
+                    self.singleList[i], self.singleList[i + 1] = self.singleList[i + 1], self.singleList[i]
+
+        return self.singleList
+
 
 
 
 list1 = ssl()
+list1.insert(5)
+list1.insert(3)
 list1.insert(1)
 list1.insert(2)
-list1.insert(3)
 list1.insert(4)
-list1.insert(5)
 print(list1.traverse())
+print(list1.sort())
